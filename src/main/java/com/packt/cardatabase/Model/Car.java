5361261913bc3b7c2d34dbe8535fafaa7d0437e2
Odney Bleus <odneybleus@Odneys-MacBook-Air.local>
2026-06-16 23:36:25 -0400

    create car model class with jpa annotations, constructor , getter and setter methods

--- src/main/java/com/packt/cardatabase/Model/Car.java
+++ src/main/java/com/packt/cardatabase/Model/Car.java
@@ -1,4 +1,95 @@
 package com.packt.cardatabase.Model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Car {
+    public Car() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Car(Long id, String brand, String model, String color, String registrationNumber, int modelYear, int price) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.modelYear = modelYear;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    private String brand;
+
+    private String model;
+
+    private String color;
+
+    private String registrationNumber;
+
+    private int modelYear;
+
+    private int price;
 }
